0e477f6be4da4a5031d4cccaabab39a3c31de2a2
Mucyo Elie Joseph <147175412+mucyoelie@users.noreply.github.com>
2025-04-13 12:55:46 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php 
+        $sentence = "The main body of a book";
+        $uppercaseSentence = strtoupper($sentence);
+        echo $uppercaseSentence . "<br><br>";
+        $newSentence = str_replace("book", "collage", $sentence);
+        echo  $newSentence . "<br><br>";
+        $length = strlen($sentence);
+        echo $length . "<br><br>";
+        for ($i = 0; $i < strlen($sentence); $i++) {
+            if ($sentence[$i] === 'm') {
+                echo $sentence[$i] . "<br>";
+            }
+        }
+        ?>
+    </p>
+</body>
+</html>
